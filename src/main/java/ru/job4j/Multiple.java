a2edc4a6bfbcb5997aaa28677fc96032b507bb85
Laszlo317 <iwannabethecoder413@gmail.com>
2022-09-24 08:31:13 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("0 * 1 = 1");
+        System.out.println("0 * 2 = 2");
+        System.out.println("0 * 3 = 3");
+        System.out.println("0 * 4 = 4");
+        System.out.println("0 * 5 = 5");
+        System.out.println("0 * 6 = 6");
+        System.out.println("0 * 7 = 7");
+        System.out.println("0 * 8 = 8");
+        System.out.println("0 * 9 = 9");
+
+    }
+}
