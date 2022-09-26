711e4868cf366444f8ecd509ca2ab29eba97b9b5
Laszlo317 <iwannabethecoder413@gmail.com>
2022-09-27 00:38:32 +0300

    5.7. Ипотека [#156315 #346716]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        percent /= 100;
-        for (; amount > 0; amount -= salary) {
-            amount += amount * percent;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
